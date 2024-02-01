13d54fde08333d8adccc861049a4b6c42aae8300
Nora Tseng <nora@tseng.us>
2024-01-31 16:39:34 -0800

    first commit: zeroshot complete, fine-tuning llama model WIP

---
+++ README.md
@@ -0,0 +1,54 @@
+# Llama 2 7b Base Model
+# With Alpaca-LoRA Fine-Tuning (PEFT method)
+# on prediction of essay understanding of PE,KE,LCE 
+
+### STATUS REPORT: Need to implement same 5fold CV as other models (reproducibility), 
+## continue fine-tuning, create separate models for CU0, CU1, ... CU5.
+
+### File explanations
+1. essay_predict_baseline.py = zeroshot predictions
+2. split_dataset.py = split training and testing data to csv for reproducibility and testing.
+3. dataset_to_train_json.py = turns training data into json of instructions
+3. train_model.py = fine-tuned model training
+4. test_model.py = testing
+5. f1_score_baseline/finetuned.txt = calculate metrics
+
+### Fine-Tuning Walkthrough:
+1. split_dataset.py (train test split)
+2. dataset_to_train_json.py (puts training data into instructions to run)
+3. train_model.py (fine tuning models)
+4. test_model.py  (evaluate on the test data)
+
+### TODO priority: 
+1. Set up reproducible 5Fold Cross Validation
+2. Fine-Tuning (learning rate, epoch) that reduces training loss under 0.1
+   - If training loss continues to decline with 25 epochs, increase epochs. 
+   - if training loss fluctuates up and down (check on wandb), reduce learning rate.
+3. Prompt engineering, make sure that training instruction is same as testing
+
+
+### TODO priority: 
+4. Set up git repository, makes things easier
+5. clean up code, comment documentation
+6. move code into jupyter notebook
+
+
+### Training/Finetuning Models
+# sample command in terminal:
+python3 train_model.py --base_model='meta-llama/Llama-2-7b-hf' --output_dir='models/essay_model14' --num_epochs=65 --data_path='train_data2.json' --val_set_size=1 --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' --learning_rate=0.005 --cutoff_len=2048
+# parameters explained: 
+   + --output_dir saves new fine-tuned model
+   + --num_epochs = iterations model learns from data
+   + --data_path = json file of instructions, 
+   + --val_set_size = size of eval/test data
+   + --lora_target_modules = loss function, each represents a linear layer
+   + --learning_rate = step size to minimize loss 
+   + --cutoff_len = cutoff for data
+
+
+##### Fine-tuning ITERATIONS
+## 1/29 train with 65 epochs, adjust learning rate: 
+# 65 epochs may be too overfitting
+python3 train_model.py --base_model='meta-llama/Llama-2-7b-hf' --output_dir='models/essay_model14' --num_epochs=65 --data_path='train_data2.json' --val_set_size=1 --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' --learning_rate=0.005 --cutoff_len=1000 
+; python3 train_model.py --base_model='meta-llama/Llama-2-7b-hf' --output_dir='models/essay_model15' --num_epochs=65 --data_path='train_data2.json' --val_set_size=1 --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' --learning_rate=0.004 --cutoff_len=1000 
+; python3 train_model.py --base_model='meta-llama/Llama-2-7b-hf' --output_dir='models/essay_model16' --num_epochs=65 --data_path='train_data2.json' --val_set_size=1 --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' --learning_rate=0.006 --cutoff_len=1000 
---
+++ test_model.py
@@ -0,0 +1,217 @@
+import torch
+from peft import PeftModel
+import transformers
+import gradio as gr
+import re
+import pandas
+import csv
+
+
+assert (
+    "LlamaTokenizer" in transformers._import_structure["models.llama"]
+), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
+from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
+
+BASE_MODEL = "meta-llama/Llama-2-7b-hf"
+LORA_WEIGHTS = "models/essay_model14"
+
+tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:
+    pass
+
+if device == "cuda":
+    model = LlamaForCausalLM.from_pretrained(
+        BASE_MODEL,
+        load_in_8bit=False,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    model = PeftModel.from_pretrained(
+        model, LORA_WEIGHTS, torch_dtype=torch.float16, force_download=True, 
+    )
+elif device == "mps":
+    model = LlamaForCausalLM.from_pretrained(
+        BASE_MODEL,
+        device_map={"": device},
+        torch_dtype=torch.float16,
+    )
+    model = PeftModel.from_pretrained(
+        model,
+        LORA_WEIGHTS,
+        device_map={"": device},
+        torch_dtype=torch.float16,
+    )
+else:
+    model = LlamaForCausalLM.from_pretrained(
+        BASE_MODEL, device_map={"": device}, low_cpu_mem_usage=True
+    )
+    model = PeftModel.from_pretrained(
+        model,
+        LORA_WEIGHTS,
+        device_map={"": device},
+    )
+
+def remove_unwanted_symbols(s):
+    new_column = ""
+    for c in s:
+        if c.isalnum() or c.isspace() or c=='_' or c==',' or c=='=' or c=="." \
+                or c=='<' or c=='>' or c=="'" or c=="(" or c==")" or c=="*" or c=="!" \
+                or c=='-' or c=='%' or c=='/' or c=='[' or c==']':
+            new_column += c
+        else:
+            #print("stop")
+            #print(c)
+            break
+    return new_column
+
+def translate(s):
+    column = s
+    # trimming unneeded content
+    if '+' in column:
+        column = column.split('+')[0]
+    # if '⁇' in column:
+    #     column = column.split('⁇')[0]
+    column = remove_unwanted_symbols(column)
+
+    #direct replacement
+    column = column.replace("trim_vendor_item","TrimVenItem")
+    column = column.replace("trim_vendor_item","TrimVenItem")
+    column = column.replace("source_vendor_item", "SrcVenItem")
+    column = column.replace("medline_suggested_substitute", "Medline Suggested Sub")
+    column = column.replace("parent_monthly_inventory_on_hand", "Parent MIOH")
+    column = column.replace("invoice_paid_total", "InvcPaidTotal")
+    # table rename
+    column = column.replace("Purchase_Order_System", "POS")
+    column = column.replace("Potential_Back_Order","PBO_Sam_PVA")
+    column = column.replace("Supply_Items_At_Risk", "SupplyItemsAtRisk")
+    column = column.replace("Purchase_Order_Receipts", "Receipts")
+
+    #indirect replacement
+    column = column.replace("source","src")
+    column = column.replace("purchase_order","PO")
+    column = column.replace("account","acct")
+    column = column.replace("description","desc")
+    column = column.replace("vendor", "vend")
+    column = column.replace("transaction", "trx")
+    column = column.replace("quantity","qty")
+    column = column.replace("manufacturer","manuf")
+    column = column.replace("process","proc")
+    column = column.replace("recommended","rec")
+    column = column.replace("location", "loc")
+    column = column.replace("request", "Req")
+    column = column.replace("monthly_inventory_on_hand", "MIOH")
+    column = column.replace("substitute", "sub")
+    column = column.replace("srcReqer", "SrcRequester")
+    if "FROM PBO_Sam_PVA" in column:
+        column = column.replace("_"," ")
+    else:
+        column = column.replace("_","")
+    return column
+
+
+def generate_prompt(instruction, input=None):
+    if input:
+        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+### Instruction:
+{instruction}
+### Input:
+{input}
+### Response:"""
+    else:
+        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+### Instruction:
+{instruction}
+### Response:"""
+
+if device != "cpu":
+    model.half()
+model.eval()
+if torch.__version__ >= "2":
+    model = torch.compile(model)
+
+
+def evaluate(
+    instruction,
+    input=None,
+    temperature=1.0,
+    top_p=0.75,
+    top_k=40,
+    num_beams=4,
+    max_new_tokens=15,
+    **kwargs,
+):
+    with torch.autocast("cuda"):
+        prompt = generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt") 
+        # inputs = tokenizer(prompt, max_length=2048 , return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        return output.split("### Response:")[1].strip()
+
+
+if __name__ == "__main__":
+    csvFile = pandas.read_csv('test_data.csv')
+    labels = ["Potential Energy", "Kinetic Energy", "Law of Conservation of Energy"]
+    label_col_names = ["PE", "KE", "LCE"]
+    input_examples = csvFile["Essay"]
+    with open('predict_output_finetune.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field = ["Essay ID","Essay","PE_Predicted","KE_Predicted","LCE_Predicted", "PE_Actual","KE_Actual","LCE_Actual"]
+        writer.writerow(field)
+
+        for row in range(len(input_examples)):
+            # if row >= 20:
+            #     break
+            input = input_examples[row]
+            print()
+            # print("Essay= " + input)
+            predicted = ["unacceptable","unacceptable","unacceptable"]
+            for i, label in enumerate(labels):
+                ### NOTE: make sure that training instruction is same as testing instr
+                instruct = "Does the following essay explain the concept from the input correctly? Yes or no. " + input
+
+                s = evaluate(instruction = instruct, input = label)
+                
+                clean_response = remove_unwanted_symbols(s)
+                if csvFile[label_col_names[i]][row].lower() == "acceptable":
+                    expected = "Yes"
+                else:
+                    expected = "No"
+                print("Label:" + label + ", Expected:" + expected + ", Response:" + clean_response)
+                
+                if "yes" in clean_response.lower():
+                    predicted[i] = "acceptable"
+
+            writer.writerow([csvFile["Essay ID"][row], input, predicted[0], predicted[1], predicted[2], csvFile["PE"][row],csvFile["KE"][row],csvFile["LCE"][row]])
+        
+
+
+
+
+def evaluate_prompt_and_instruction(instruction, input):
+    return evaluate(instruction, input)
